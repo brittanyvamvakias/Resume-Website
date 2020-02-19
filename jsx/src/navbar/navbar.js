cc4b066cce8e3419acadc69e6c6b2bdd933de607
Brittany Vamvakias <58999101+brittanyvamvakias@users.noreply.github.com>
2020-02-19 15:33:06 -0600

    Began creation of CSS classes for the navigation bar

---
+++ jsx/src/navbar/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function Navbar() {
+    return (
+        <nav ClassName="primary-navbar">
+            <span className="navbar-logo">
+                Brittany Vamvakias' Resumé
+            </span>
+        </nav>
+    )
+}
+export default Navbar;
